3c7cc7877fed16b96c1e136951f56b352037908c
Rich Mays <richmays@gmail.com>
2021-09-23 15:12:31 -0400

    Added the board and move picker logic.  Each player has a button for each possible move.  TODO: Detect checkmate / stalemate and position repetiton (draw).  Display the result of the game.

--- RMays.ChessEncode/ChessEncodeUI.cs
+++ RMays.ChessEncode/ChessEncodeUI.cs
@@ -15,10 +15,12 @@ namespace RMays.ChessEncode
 {
     public partial class ChessEncodeUI : Form
     {
+        public event EventHandler MoveButtonClicked;
         protected PictureBox[,] squares = new PictureBox[8,8];
         private Bitmap darkSquares;
         private Bitmap lightSquares;
         private ResourceManager resourceManager;
+        private MovePicker movePicker;
 
         private int squareLength => Math.Max(GetInt(txtSquareSideLength), 1);
         private int imgLeftOffset => GetInt(txtImgLeftOffset);
@@ -40,7 +42,23 @@ namespace RMays.ChessEncode
             darkSquares = (Bitmap)resourceManager.GetObject("chesspieces_darkBG");
             lightSquares = (Bitmap)resourceManager.GetObject("chesspieces_whiteBG");
 
+            //board.TryMakeMove(0);
+            //board.TryMakeMove(0);
+
             PrintBoard();
+            ResetMovePicker();
+        }
+
+        private void ResetMovePicker()
+        {
+            this.movePicker = new MovePicker { Owner = this };
+            this.movePicker.Show();
+            this.movePicker.RefreshMoves(board);
+        }
+
+        public void HandleMove(object sender, EventArgs e)
+        {
+
         }
 
         private Dictionary<string, (int, int)> PieceImageOffsets = new Dictionary<string, (int, int)>
@@ -70,12 +88,18 @@ namespace RMays.ChessEncode
                     var piece = board.GetSpot(f, r);
                     var offsets = PieceImageOffsets[piece.ToString()];
 
-                    if (squares[f - 1, r - 1] != null)
+                    // To prevent excessive flashing (64x -> 2x), only replace the images that actually changed.
+                    var newPiece = GetPieceWithOffset(f, r, offsets.Item1, offsets.Item2, piece.ToString());
+                    var currSquare = squares[f - 1, r - 1];
+                    if (currSquare == null)
+                    {
+                        squares[f - 1, r - 1] = newPiece;
+                    }
+                    else if (currSquare.Tag != newPiece.Tag)
                     {
                         squares[f - 1, r - 1].Dispose();
+                        squares[f - 1, r - 1] = newPiece;
                     }
-
-                    squares[f - 1, r - 1] = GetPieceWithOffset(f, r, offsets.Item1, offsets.Item2);
                 }
             }
             this.ResumeLayout();
@@ -95,16 +119,17 @@ namespace RMays.ChessEncode
                         squares[f - 1, r - 1].Dispose();
                     }
 
-                    squares[f - 1, r - 1] = GetPieceWithOffset(f, r, r, f);
+                    squares[f - 1, r - 1] = GetPieceWithOffset(f, r, r, f, "Blank");
                 }
             }
 
             this.ResumeLayout();
         }
 
-        private PictureBox GetPieceWithOffset(int f, int r, int row, int col)
+        private PictureBox GetPieceWithOffset(int f, int r, int row, int col, string label)
         {
             var pb = new PictureBox();
+            pb.Tag = label;
             pb.BackgroundImage = cropImage((f + r) % 2 == 1 ? lightSquares : darkSquares,
                 new Rectangle(
                     this.imgHPadding * col + this.imgLeftOffset,
@@ -180,6 +205,45 @@ namespace RMays.ChessEncode
         private void txtBoardTopOffset_ValueChanged(object sender, EventArgs e)
         {
             PrintBoard();
+        }
+
+        private void btnToggleBoardLayoutNumbers_Click(object sender, EventArgs e)
+        {
+            ToggleVisibility(label1, label2, label3, label4, label5, label6, label7, label8, label9, label10, label11, label11,
+                txtSquareSideLength,
+                txtImgHPadding, txtImgVPadding, txtImgLeftOffset, txtImgTopOffset,
+                txtBoardHPadding, txtBoardVPadding, txtBoardTopOffset, txtBoardLeftOffset);
+        }
+
+        private void ToggleVisibility(params Control[] controls)
+        {
+            if (controls.Length == 0) return;
+            var targetVisibility = !controls[0].Visible;
+            foreach (Control control in controls)
+            {
+                control.Visible = targetVisibility;
+            }
+        }
+
+        private void btnToggleMovePicker_Click(object sender, EventArgs e)
+        {
+            if (movePicker == null)
+            {
+                ResetMovePicker();
+            }
+            else
+            {
+                movePicker.Visible = !movePicker.Visible;
+            }
+        }
+
+        internal void btnMakeMoveButton_Click(object sender, EventArgs e)
+        {
+            var moveId = int.Parse(((Button)sender).Tag.ToString());
+            board.TryMakeMove(moveId);
+            PrintBoard();
+            this.movePicker.RefreshMoves(board);
+            //MessageBox.Show($"Move: {moveText}");
         }
     }
 }
---
+++ RMays.ChessEncode/MovePicker.cs
@@ -0,0 +1,88 @@
+﻿using Rmays.ChessEngine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RMays.ChessEncode
+{
+    public partial class MovePicker : Form
+    {
+        public MovePicker()
+        {
+            InitializeComponent();
+            this.Hide();
+
+            //AddButtons(250);
+        }
+
+        private void AddButtons(int buttonsToAdd)
+        {
+            for (var buttonId = 0; buttonId < buttonsToAdd; buttonId++)
+            {
+                var button = new Button { Name = $"button{buttonId}", Text = "?", Tag = buttonId.ToString() };
+                button.Top = (buttonId / 5) * 24 + 50;
+                button.Left = (buttonId % 5) * 90 + 10;
+                button.Click += new System.EventHandler(((ChessEncodeUI)Owner).btnMakeMoveButton_Click);
+                button.Hide();
+                Controls.Add(button);
+            }
+        }
+
+        public void RefreshMoves(ChessBoardState board)
+        {
+            if (!Controls.Find("button1", false).Any())
+            {
+                AddButtons(250);
+            }
+            
+            // Update the active player.
+            this.label1.Text = (board.GetSideToMove() == ChessColor.White ? "White's turn" : "Black's turn");
+
+            // Hide all buttons.
+            foreach (Button button in Controls.OfType<Button>())
+            {
+                button.Hide();
+            }
+
+            /*
+            int safety = 0;
+            while(controlsToRemove.Any() && safety < 1000)
+            {
+                Controls.Remove(controlsToRemove.First());
+                safety++;
+            }
+
+            if (safety >= 1000)
+            {
+                throw new ApplicationException("Caught in an infinite loop removing controls in MovePicker.");
+            }
+            */
+
+            // Show the buttons that correspond with possible moves.
+            var movesAdded = 0;
+            foreach(var move in board.PossibleMoves())
+            {
+                var button = (Button)Controls.Find($"button{movesAdded}", false).First();
+                if (button == null)
+                {
+                    throw new ApplicationException($"Couldn't find the control 'button{movesAdded}'.");
+                }
+                button.Text = move.ToString();
+                button.Show();
+                /*var button = new Button { Name = "button1", Text = move.ToString(), Tag = movesAdded.ToString() };
+                button.Top = (movesAdded / 5) * 24 + 50;
+                button.Left = (movesAdded % 5) * 90 + 10;
+                button.Click += new System.EventHandler(((ChessEncodeUI)Owner).btnMakeMoveButton_Click);
+                Controls.Add(button);
+                */
+                movesAdded++;
+            }
+        }
+    }
+}
